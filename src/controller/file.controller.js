a0674d80e0c0eaa87e84f72c8ef2f5486dc97db7
Yerbol Tastanov <armani910@gmail.com>
2021-06-24 11:41:38 +0600

    first commit was made

---
+++ src/controller/file.controller.js
@@ -0,0 +1,94 @@
+const uploadFile = require("../middleware/upload");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+const upload = async (req, res) => {
+    try {
+        await uploadFile(req, res);
+
+        if (req.file == undefined) {
+            return res.status(400).send({ message: "Please upload a file!", success: false });
+        }
+        const originalFilename = req.file.originalname;
+        const fileName = req.file.filename;
+        console.log(req.file);
+
+        res.status(200).send({
+            name: fileName,
+            orginalName: originalFilename,
+            success: true
+        });
+    } catch (err) {
+        res.status(500).send({
+            message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+            success: false
+        });
+    }
+};
+
+const getListFiles = (req, res) => {
+    // const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+    // fs.readdir(directoryPath, function (err, files) {
+    //     if (err) {
+    //         res.status(500).send({
+    //             message: "Unable to scan files!"
+    //         });
+    //     }
+
+    //     let fileInfos = [];
+
+    //     files.forEach(file => {
+    //         fileInfos.push({
+    //             name: file,
+    //             url: baseUrl + file
+    //         });
+    //     });
+
+    //     res.status(200).send(fileInfos);
+    // });
+    res.status(401).send({
+        message: "Unauthorized, no credentials"
+    });
+};
+
+const download = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+    res.download(directoryPath + fileName, fileName, err => {
+        if (err) {
+            res.status(500).send({
+                message: "Could not download the file. " + err,
+                success: false
+            });
+        }
+    });
+};
+
+const deleteFile = (req, res) => {
+    const fileName = req.params.name;
+    const directoryPath = __basedir + `/resources/static/assets/uploads/${fileName}`;
+
+    fs.unlink(directoryPath, err => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({
+                success: false,
+                message: "Server error has been occured"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "File was removed"
+        });
+    });
+};
+
+module.exports = {
+    upload,
+    getListFiles,
+    download,
+    deleteFile
+};
---
+++ src/middleware/upload.js
@@ -0,0 +1,30 @@
+const util = require("util");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const maxSize = 2 * 1024 * 1024;
+
+let originalNameOfFile = "";
+let newNameOfFile = "";
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __basedir + "/resources/static/assets/uploads/");
+    },
+
+    filename: (req, file, cb) => {
+        originalNameOfFile = file.originalname;
+        const lastDot = originalNameOfFile.lastIndexOf(".");
+        const ext = originalNameOfFile.substring(lastDot + 1);
+        const uniqueName = uuidv4();
+        newNameOfFile = (uniqueName + "." + ext).trim();
+        cb(null, newNameOfFile);
+    }
+});
+
+let uploadFile = multer({
+    storage: storage,
+    limits: { fileSize: maxSize }
+}).single("file");
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
