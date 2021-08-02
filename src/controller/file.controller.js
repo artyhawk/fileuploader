f3ca3747fb060fab27c42dadfbde4e9cc4892551
Yerbol Tastanov <armani910@gmail.com>
2021-08-02 18:48:26 +0600

    /api/pdf/:name was added

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -3,92 +3,100 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
 const upload = async (req, res) => {
-    try {
-        const originalFilename = req.params.originalName;
-        await uploadFile(req, res);
+  try {
+    const originalFilename = req.params.originalName;
+    await uploadFile(req, res);
 
-        if (req.file == undefined) {
-            return res.status(400).send({ message: "Please upload a file!", success: false });
-        }
-
-        const fileName = req.file.filename;
-
-        res.status(200).send({
-            name: fileName,
-            orginalName: originalFilename,
-            success: true
-        });
-    } catch (err) {
-        res.status(500).send({
-            message: `Could not upload the file: ${req.params.originalName}. ${err.message}`,
-            success: false
-        });
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Please upload a file!", success: false });
     }
+
+    const fileName = req.file.filename;
+
+    res.status(200).send({
+      name: fileName,
+      orginalName: originalFilename,
+      success: true
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: `Could not upload the file: ${req.params.originalName}. ${err.message}`,
+      success: false
+    });
+  }
 };
 
 const getListFiles = (req, res) => {
-    // const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  // const directoryPath = __basedir + "/resources/static/assets/uploads/";
 
-    // fs.readdir(directoryPath, function (err, files) {
-    //     if (err) {
-    //         res.status(500).send({
-    //             message: "Unable to scan files!"
-    //         });
-    //     }
+  // fs.readdir(directoryPath, function (err, files) {
+  //     if (err) {
+  //         res.status(500).send({
+  //             message: "Unable to scan files!"
+  //         });
+  //     }
 
-    //     let fileInfos = [];
+  //     let fileInfos = [];
 
-    //     files.forEach(file => {
-    //         fileInfos.push({
-    //             name: file,
-    //             url: baseUrl + file
-    //         });
-    //     });
+  //     files.forEach(file => {
+  //         fileInfos.push({
+  //             name: file,
+  //             url: baseUrl + file
+  //         });
+  //     });
 
-    //     res.status(200).send(fileInfos);
-    // });
-    res.status(401).send({
-        message: "Unauthorized, no credentials"
-    });
+  //     res.status(200).send(fileInfos);
+  // });
+  res.status(401).send({
+    message: "Unauthorized, no credentials"
+  });
 };
 
 const download = (req, res) => {
-    const fileName = req.params.name;
-    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  const fileName = req.params.name;
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
 
-    res.download(directoryPath + fileName, fileName, err => {
-        if (err) {
-            res.status(500).send({
-                message: "Could not download the file. " + err,
-                success: false
-            });
-        }
-    });
+  res.download(directoryPath + fileName, fileName, err => {
+    if (err) {
+      res.status(500).send({
+        message: "Could not download the file. " + err,
+        success: false
+      });
+    }
+  });
+};
+
+const showPdf = (req, res) => {
+  const fileName = req.params.name;
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+  res.sendFile(directoryPath + fileName);
 };
 
 const deleteFile = (req, res) => {
-    const fileName = req.params.name;
-    const directoryPath = __basedir + `/resources/static/assets/uploads/${fileName}`;
+  const fileName = req.params.name;
+  const directoryPath = __basedir + `/resources/static/assets/uploads/${fileName}`;
 
-    fs.unlink(directoryPath, err => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({
-                success: false,
-                message: "Server error has been occured"
-            });
-        }
+  fs.unlink(directoryPath, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        success: false,
+        message: "Server error has been occured"
+      });
+    }
 
-        res.status(200).json({
-            success: true,
-            message: "File was removed"
-        });
+    res.status(200).json({
+      success: true,
+      message: "File was removed"
     });
+  });
 };
 
 module.exports = {
-    upload,
-    getListFiles,
-    download,
-    deleteFile
+  upload,
+  getListFiles,
+  download,
+  deleteFile,
+  showPdf
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const controller = require("../controller/file.controller");
 
 let routes = app => {
-    router.post("/api/upload/:originalName", controller.upload);
-    router.get("/api/files", controller.getListFiles);
-    router.get("/api/files/:name", controller.download);
-    router.delete("/api/files/:name", controller.deleteFile);
-    app.use(router);
+  router.post("/api/upload/:originalName", controller.upload);
+  router.get("/api/files", controller.getListFiles);
+  router.get("/api/files/:name", controller.download);
+  router.delete("/api/files/:name", controller.deleteFile);
+  router.get("/api/pdf/:name", controller.showPdf);
+  app.use(router);
 };
 
 module.exports = routes;
